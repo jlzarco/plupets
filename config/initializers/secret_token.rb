6af36996f650d80b62a0c5bbfffa9b65f26204d9
ricardo <ricardocruz.a89@gmail.com>
2012-07-20 16:28:38 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plupets::Application.config.secret_token = '788c0cd303799aea41f19c7be1d0641ed55d6eae4b916873ad761fe4fede130d18983c8a46782d55400819d4e4c1b494f62f40f56c30b24bd51de4df33954252'
