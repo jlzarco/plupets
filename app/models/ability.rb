ccd73cbab84ff787bfc9ac1192813a5ff59e215d
ricardo <ricardocruz.a89@gmail.com>
2012-09-03 18:50:43 -0500

    arreglando heroku

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,21 +4,20 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-      user ||= User.new # guest user (not logged in)
-       if user.role? :admin
-         can :manage, :all
-       elsif user.role? :loged
-          can :read, :all
-         # can :create, Comment
-        #  can :update, Comment do|comment|
-        #    comment.try(:user)==user 
-          end
-       elsif user.role? :info
-         can :read, User
-       else
-         can :read, Pet
-         
-       end
+    user ||= User.new # guest user (not logged in)
+    if user.role? :admin
+      can :manage, :all
+    elsif user.role? :loged
+      can :read, :all
+      # can :create, Comment
+      #  can :update, Comment do|comment|
+      #    comment.try(:user)==user
+    elsif user.role? :info
+      can :read, User
+    else
+      can :read, Pet
+
+    end
     #
     # The first argument to `can` is the action you are giving the user permission to do.
     # If you pass :manage it will apply to every action. Other common actions here are
@@ -33,5 +32,5 @@ class Ability
     #   can :update, Article, :published => true
     #
     # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
-end
+  end
 end
